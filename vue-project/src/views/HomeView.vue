cd6f82640554140bf996edf2eb441fd60f0808f8
Thomas Anderson <tanderson@C56.local>
2022-03-09 11:00:57 -0500

    trying gsap

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <main>
-    <div v-for="post in store.state.posts" :key="post.name">
-      <h2>{{ post.title }}</h2>
+    <div v-for="post in store.state.posts" :key="post.name" class="post">
+      <h2 ref="post-title">{{ post.title }}</h2>
       <h4>by <router-link :to="`/user/${post.author.uid}`">{{ post.author.dname }}</router-link></h4> 
-      <p>{{ post.content }}</p>
+      <p ref="post-content">{{ post.content }}</p>
     </div>
   </main>
 </template>
@@ -13,7 +13,12 @@
   const store = useStore()
   store.dispatch("getPosts")
 </script>
-
+<script>
+ import { gsap } from "gsap"
+ const tl = gsap.timeline({ delay: 0.2 });
+  tl.from(this.$refs.post-title, { opacity: 0, duration: 1.5 });
+  tl.from(this.$refs.post-content, { opacity: 0, duration: 1.25 });
+</script>
 <style scoped>
 
 </style>
