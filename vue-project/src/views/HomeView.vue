dc118554c3086cc549b9a329bcb79b6f010257b1
Thomas Anderson <tanderson@C56.local>
2022-03-11 10:39:29 -0500

    transitions

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,24 +1,64 @@
 <template>
-  <main>
+  <main>  
     <div v-for="post in store.state.posts" :key="post.name" class="post">
-      <h2 ref="post-title">{{ post.title }}</h2>
+      <transition
+      appear
+      @before-enter="beforeEnter"
+      @enter="headerEnter"
+      > 
+      <h2>{{ post.title }}</h2> 
+      </transition>
+        <transition
+      appear
+      @before-enter="beforeEnter"
+      @enter="subtextEnter"
+      > 
       <h4>by <router-link :to="`/user/${post.author.uid}`">{{ post.author.dname }}</router-link></h4> 
-      <p ref="post-content">{{ post.content }}</p>
+        </transition>
+       <transition
+      appear
+      @before-enter="beforeEnter"
+      @enter="bodyEnter"
+      > 
+      <p>{{ post.content }}</p>
+       </transition>
     </div>
   </main>
 </template>
 
 <script setup>
-  import { useStore } from 'vuex'
-  const store = useStore()
-  store.dispatch("getPosts")
+  import { useStore } from 'vuex' 
+  import gsap from 'gsap'
+  const store = useStore();  
+  store.dispatch("getPosts");
+  const beforeEnter = (el) => {
+        console.log("test1");
+        el.style.opacity = 0
+      }
+      const headerEnter = (el) => {
+        console.log("test2");
+        gsap.to(el,{
+          duration:1.5,
+          opacity:1,
+        })
+      }
+      const bodyEnter = (el) => {
+       setTimeout(
+        function() {gsap.to(el,{
+          duration: 2,
+          opacity: 1,
+        })}, 1000)
+      }  
+      const subtextEnter = (el) => {
+       setTimeout(
+        function() {gsap.to(el,{
+          duration: 2,
+          opacity: 1,
+        })}, 750)
+      }  
 </script>
-<script>
- import { gsap } from "gsap"
- const tl = gsap.timeline({ delay: 0.2 });
-  tl.from(this.$refs.post-title, { opacity: 0, duration: 1.5 });
-  tl.from(this.$refs.post-content, { opacity: 0, duration: 1.25 });
-</script>
+
+
 <style scoped>
 
 </style>
