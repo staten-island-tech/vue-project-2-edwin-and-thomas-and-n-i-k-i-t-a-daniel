1e4c8aaf212d399303a0a6cbd308253c2b8e0057
ewright516 <ewright51606@gmail.com>
2022-03-17 10:55:29 -0400

    working on post page

--- vue-project/src/views/Post.vue
+++ vue-project/src/views/Post.vue
@@ -1,18 +1,33 @@
 <template>
-    <div>
-        <h2>{{ post.title }}</h2>
+    <div class="main">
+        <div class="radio">
+            <h3 class="radio-item" @click="radio = 'post'">Post</h3>
+            <h3 class="radio-item" @click="radio = 'comments'">Comments</h3>
+        </div>
+
+        <div v-if="radio === 'post'">
+            <h2>{{ post.title }}</h2>
+            <h4>by {{ post.author.dname }}</h4>
+            <div id="content" v-html="post.content"></div>
+        </div>
+
+        <div v-if="radio === 'comments'">
+            <h2>Comments</h2>
+        </div>
     </div>
 </template>
 
 <script setup>
 import { useStore } from 'vuex'
 import { useRoute } from 'vue-router';
-import { computed, watch } from 'vue';
+import { computed, watch, ref } from 'vue';
 const route = useRoute()
 const store = useStore()
 const user = computed(() => store.state.user)
 const post = computed(() => store.state.posts[0])
 store.dispatch("getSinglePost", route.params.id)
+
+const radio = ref('post')
 
 watch(
     () => route.params.id,
@@ -23,5 +38,33 @@ watch(
 </script>
 
 <style scoped>
+
+.main {  
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+}
+.radio {
+    display: flex;
+    flex-flow: row nowrap;
+}
+.radio-item {
+    cursor: pointer;
+    background-color: #794d4d51;
+    color: #724949;
+    border: none;
+    border-radius: 2rem;
+    height: 5rem;
+    padding: .6rem 1.6rem;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    margin-right: 0.5rem;
+    margin-left: 0.5rem;
+    text-align: center;
+    text-decoration-line: none;
+}
+#content {
+    font-size: 2rem;
+}
 
 </style>
